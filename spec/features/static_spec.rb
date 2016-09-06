6f6337a34951c5ebe15dc297291a649128a9952a
jordanhudgens <jordan@devcamp.com>
2016-09-06 22:33:43 +0000

    Implemented fixes for the test suite configuration

--- spec/features/static_spec.rb
+++ spec/features/static_spec.rb
@@ -1,9 +1,10 @@
 require 'rails_helper'
 
 describe 'navigate' do
-  it 'can be reached successfully' do
-    visit root_path
-    expect(page.status_code).to eq(200)
+  describe 'homepage' do
+    it 'can be reached successfully' do
+      visit root_path
+      expect(page.status_code).to eq(200)
+    end
   end
-end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,21 +1,9 @@
-ENV['RAILS_ENV'] ||= 'test'
-require File.expand_path('../../config/environment', __FILE__)
-
-abort("The Rails environment is running in production mode!") if Rails.env.production?
-require 'spec_helper'
-require 'rspec/rails'
-require 'capybara/rails'
-
-ActiveRecord::Migration.maintain_test_schema!
-
 RSpec.configure do |config|
-  config.fixture_path = "#{::Rails.root}/spec/fixtures"
-  config.use_transactional_fixtures = false
-  config.before(:suite) { DatabaseCleaner.clean_with(:truncation) }
-  config.before(:each) { DatabaseCleaner.strategy = :transaction }
-  config.before(:each, :js => true) { DatabaseCleaner.strategy = :truncation }
-  config.before(:each) { DatabaseCleaner.start }
-  config.after(:each) { DatabaseCleaner.clean }
-  config.infer_spec_type_from_file_location!
-  config.filter_rails_from_backtrace!
+  config.expect_with :rspec do |expectations|
+    expectations.include_chain_clauses_in_custom_matcher_descriptions = true
+  end
+
+  config.mock_with :rspec do |mocks|
+    mocks.verify_partial_doubles = true
+  end
 end
