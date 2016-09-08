77a81869872d2015f5aad84c0e471dde6855c495
Ryan Bent <ryanbent01@gmail.com>
2016-09-07 22:06:34 -0500

    Add validations

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe User, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+ describe 'creation' do
+  before do
+    @user = User.create(email: 'test@test.com', password: 'testing123', password_confirmation: 'testing123', first_name:'rico', last_name: 'ross')
+  end
+  it 'can be created' do
+    expect(@user).to be_valid
+  end
+
+    it 'cannot be created without first_name, last_name' do
+      @user.first_name = nil
+      @user.last_name = nil
+      expect(@user).to_not be_valid
+    end
+ end
 end
