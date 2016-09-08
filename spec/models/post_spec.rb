d23d22d32ed8b6a729071b0e7dd9c4e054d6c2a0
Ryan Bent <ryanbent01@gmail.com>
2016-09-07 22:30:40 -0500

    Add post resources

---
+++ spec/models/post_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+RSpec.describe Post, :type => :model do
+  describe 'creation' do
+    before do 
+     @post = Post.create(date: Date.today, rationale: 'vaction')
+    end 
+    it 'can be created' do
+      expect(@post).to be_valid
+    end
+
+    it 'cannot be created without a date and rationale' do
+      @post.date = nil
+      @post.rationale = nil
+      expect(@post).to_not be_valid 
+    end
+  end    
+end
