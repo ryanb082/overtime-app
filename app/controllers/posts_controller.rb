0b87f9af6f8a759224681cecad2661726084b397
Ryan Bent <ryanbent01@gmail.com>
2016-09-07 23:46:10 -0500

    add post feature

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,4 +2,27 @@ class PostsController < ApplicationController
 
   def index
   end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+
+    @post.save
+
+    redirect_to @post
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+
+  private
+
+  def post_params
+    params.require(:post).permit(:date, :rationale)
+  end
 end
--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -12,4 +12,22 @@ describe 'navigate' do
       expect(page).to have_content(/Posts/)
     end
   end
+
+  describe 'creation' do
+    it 'has a new form that can be reached' do
+      visit new_post_path
+      expect(page.status_code).to eq(200)
+    end
+
+    it 'can be created from new form page' do
+      visit new_post_path
+
+      fill_in 'post[date]', with: Date.today
+      fill_in 'post[rationale]', with: 'Some rationale'
+
+      click_on 'Save'
+
+      expect(page).to have_content('Some rationale')
+    end
+  end
 end
