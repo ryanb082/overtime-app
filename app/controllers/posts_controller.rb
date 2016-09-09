9b86e1701bda462021b688d6bf4b6860ad4aeafa
Ryan Bent <ryanbent01@gmail.com>
2016-09-08 23:48:06 -0500

    look into rspec fail error in post feature test

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,7 @@
 class PostsController < ApplicationController
 
   def index
+    @posts = Post.all
   end
 
   def new
@@ -10,7 +11,6 @@ class PostsController < ApplicationController
   def create
     @post = Post.new(post_params)
     @post.user_id = current_user.id
-
     @post.save
 
     redirect_to @post
--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -1,22 +1,35 @@
+
 require 'rails_helper'
 
 describe 'navigate' do
+  before do
+    user = User.create(email: 'test@test.com', password: 'testing123', password_confirmation: 'testing123', first_name:'rico', last_name: 'ross')
+      login_as(user, :scope => :user)
+    end
+
   describe 'index' do
-    it 'can be reached successfully' do
+    before do
       visit posts_path
+    end
+
+    it 'can be reached successfully' do
       expect(page.status_code).to eq(200)
     end
 
     it 'has a title of Posts' do
-      visit posts_path
       expect(page).to have_content(/Posts/)
+    end
+
+    it 'has a list of posts' do
+      post1 = Post.create(date: Date.today, rationale: 'Post1')
+      post2 = Post.create(date: Date.today, rationale: 'Post2')
+      visit posts_path 
+      expect(page).to have_content(/Post1|Post2/)
     end
   end
 
   describe 'creation' do
     before do
-      user = User.create(email: 'test@test.com', password: 'testing123', password_confirmation: 'testing123', first_name:'rico', last_name: 'ross')
-      login_as(user, :scope => :user)
       visit new_post_path
     end
 
@@ -25,11 +38,8 @@ describe 'navigate' do
     end
 
     it 'can be created from new form page' do
-      visit new_post_path
-
       fill_in 'post[date]', with: Date.today
       fill_in 'post[rationale]', with: 'Some rationale'
-
       click_on 'Save'
 
       expect(page).to have_content('Some rationale')
