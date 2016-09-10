c60c426a927c3fe16e4239aca6bb3979182c93db
Ryan Bent <ryanbent01@gmail.com>
2016-09-10 15:11:11 -0500

    Added new post button to nav bar

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -28,6 +28,15 @@ describe 'navigate' do
     end
   end
 
+  describe 'new' do
+    it 'has a link from homepage' do
+      visit root_path
+
+      click_link("new_post_from_nav")
+      expect(page.status_code).to eq(200)
+    end
+  end
+
   describe 'creation' do
     before do
       visit new_post_path
