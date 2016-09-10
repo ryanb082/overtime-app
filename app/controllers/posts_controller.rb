779f1919474311dbacbb662e1dcaf70deb65dde2
Ryan Bent <ryanbent01@gmail.com>
2016-09-10 15:56:46 -0500

    Add the ablity to delete records

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,5 @@
 class PostsController < ApplicationController
-  before_action :set_post, only: [:show, :edit, :update]
+  before_action :set_post, only: [:show, :edit, :update, :destroy]
 
   def index
     @posts = Post.all
@@ -34,6 +34,11 @@ class PostsController < ApplicationController
     else
       render :edit
     end
+  end
+
+  def destroy
+    @post.delete
+    redirect_to posts_path notice: 'Your post was deleted'
   end
 
 
--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -37,6 +37,16 @@ describe 'navigate' do
     end
   end
 
+  describe 'delete' do
+    it 'can be deleted' do
+      @post = FactoryGirl.create(:post)
+      visit posts_path
+
+      click_link("delete_post_#{@post.id}from_index")
+      expect(page.status_code).to eq(200)
+    end
+  end
+
   describe 'creation' do
     before do
       visit new_post_path
