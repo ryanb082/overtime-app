64a409bf2c38bdb56a5d354de5cb4e3c35821f2f
Ryan Bent <ryanbent01@gmail.com>
2016-09-10 11:12:44 -0500

    Refactor index page to use post partial and add user_id for rspec

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -3,12 +3,13 @@ require 'rails_helper'
 
 describe 'navigate' do
   before do
-    user = User.create(email: 'test@test.com', password: 'testing123', password_confirmation: 'testing123', first_name:'rico', last_name: 'ross')
-      login_as(user, :scope => :user)
+    @user = User.create!(email: 'test@test.com', password: 'testing123', password_confirmation: 'testing123', first_name:'rico', last_name: 'ross')
+      login_as(@user, :scope => :user)
     end
 
   describe 'index' do
     before do
+
       visit posts_path
     end
 
@@ -21,8 +22,8 @@ describe 'navigate' do
     end
 
     it 'has a list of posts' do
-      post1 = Post.create(date: Date.today, rationale: 'Post1')
-      post2 = Post.create(date: Date.today, rationale: 'Post2')
+      post1 = Post.create(date: Date.today, rationale: 'Post1', user_id: @user.id)
+      post2 = Post.create(date: Date.today, rationale: 'Post2', user_id: @user.id)
       visit posts_path 
       expect(page).to have_content(/Post1|Post2/)
     end
