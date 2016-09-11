03650095bcd4152388d1b0edb71a1f9401a471c9
Ryan Bent <ryanbent01@gmail.com>
2016-09-11 14:04:57 -0500

    Add radio button functionality with passing rspec test

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -45,7 +45,7 @@ class PostsController < ApplicationController
   private
 
   def post_params
-    params.require(:post).permit(:date, :rationale)
+    params.require(:post).permit(:date, :rationale, :status)
   end
 
   def set_post
