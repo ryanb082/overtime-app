22137de9eea92de36e21f4fd99ba99c9583fc1ec
Ryan Bent <ryanbent01@gmail.com>
2016-09-10 19:02:50 -0500

    Removed registration ability from app

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,14 @@
 Rails.application.routes.draw do
   resources :posts
-  devise_for :users
+  devise_for :users, skip: [:registrations]
+  root to: 'static#homepage'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
 
-  root to: 'static#homepage'
+ 
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
