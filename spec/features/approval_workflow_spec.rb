d7c634bf0e0519798351f415fa1749052b8e5996
Ryan Bent <ryanbent01@gmail.com>
2016-09-13 23:31:53 -0500

    fix rpec ./spec/features/post_spec.rb:105

--- spec/features/approval_workflow_spec.rb
+++ spec/features/approval_workflow_spec.rb
@@ -13,8 +13,6 @@ describe 'navigate' do
     end
 
     it 'has a status that can be edited on the form by an admin' do
-      
-
       choose('post_status_approved')
       click_on 'Save'
 
@@ -29,6 +27,18 @@ describe 'navigate' do
         visit edit_post_path(@post)
 
         expect(page).to_not have_content('Approved')
+    end
+
+    it 'should not be editable by the post creator if status is approved' do
+      logout(:user)
+      user = FactoryGirl.create(:user)
+      login_as(user, :scope => :user)
+
+      @post.update(user_id: user.id, status: 'approved')
+
+      visit edit_post_path(@post)
+
+      expect(current_path).to eq(root_path)
     end
   end
 end
