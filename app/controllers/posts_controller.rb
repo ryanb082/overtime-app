1861c596c0172f2a364557ed5d37c6ace34224e8
jordanhudgens <jordan@devcamp.com>
2016-09-17 22:59:42 +0000

    Fixed policy

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,60 +1,56 @@
 class PostsController < ApplicationController
-	before_action :set_post, only: [:show, :edit, :update, :destroy, :approve]
+  before_action :set_post, only: [:show, :edit, :update, :destroy]
 
-	def index
-		@posts = Post.posts_by(current_user).page(params[:page]).per(10)
-	end
+  def index
+    @posts = Post.posts_by current_user
+  end
 
-	def approve
-		authorize @post
-		@post.approved!
-		redirect_to root_path, notice: "The post has been approved"
-	end
+  def new
+    @post = Post.new
+  end
 
-	def new
-		@post = Post.new
-	end
+  def create
+    @post = Post.new(post_params)
+    @post.user_id = current_user.id
+    if @post.save
+      redirect_to @post, notice: 'Your post was created successfully'
+    else 
+      render :new
+    end
+  end
 
-	def create
-		@post = Post.new(post_params)
-		@post.user_id = current_user.id
+  def edit
+    authorize @post
+  end
 
-		if @post.save
-			redirect_to @post, notice: 'Your post was created successfully'
-		else
-			render :new
-		end
-	end
+  def show
+    @post = Post.find(params[:id])
 
-	def edit
-		authorize @post
-	end
+  end
 
-	def update
-		authorize @post
+  def update
+    authorize @post
 
-		if @post.update(post_params)
-			redirect_to @post, notice: 'Your post was edited successfully'
-		else
-			render :edit
-		end
-	end
+    if @post.update(post_params)
+      redirect_to @post, notice: 'Your post was updated successfully'
+    else
+      render :edit
+    end
+  end
 
-	def show
-	end
-
-	def destroy
+  def destroy
     @post.delete
-    redirect_to posts_path, notice: 'Your post was deleted successfully'
-	end
+    redirect_to posts_path, notice: 'Your post was deleted'
+  end
 
-	private
 
-	  def post_params
-	  	params.require(:post).permit(:date, :rationale, :status)
-	  end
+  private
 
-	  def set_post
-	  	@post = Post.find(params[:id])
-	  end
+  def post_params
+    params.require(:post).permit(:date, :rationale, :status)
+  end
+
+  def set_post
+    @post = Post.find(params[:id])
+  end
 end
--- app/policies/post_policy.rb
+++ app/policies/post_policy.rb
@@ -1,24 +1,20 @@
 class PostPolicy < ApplicationPolicy
   def update?
-  	return true if post_approved? && admin?
-  	return true if user_or_admin && !post_approved?
-  end
-
-  def approve?
-    admin?
+    return true if post_approved?  && admin?
+    return true if user_or_admin && !post_approved?
   end
 
   private
 
-  	def user_or_admin
-  		record.user_id == user.id || admin?
-  	end
+  def user_or_admin
+    record.user_id == user.id || admin?
+  end
 
-  	def admin?
-  		admin_types.include?(user.type)
-  	end
+  def admin?
+    admin_types.include?(user.type)
+  end
 
-    def post_approved?
-    	record.approved?
-    end
+  def post_approved?
+    record.approved?
+  end
 end
