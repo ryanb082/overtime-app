f02b5dad9ccb0c6a60c1ef26f19b6aa789e7cc3d
Ryan Bent <ryanbent01@gmail.com>
2016-09-22 20:59:34 -0500

    integrated phone validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,13 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
   validates_presence_of :first_name, :last_name, :phone
+  # validates :phone, numericality: { only_integer: true }
+  # validates :phone, length: { maximum: 10 }# 
+
+  PHONE_REGEX = /\A[0-9]*\Z/
+
+  validates_format_of :phone, with: PHONE_REGEX
+  validates :phone, length: { is: 10 }
   has_many :posts
 
   def full_name
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -10,18 +10,30 @@ RSpec.describe User, :type => :model do
     expect(@user).to be_valid
   end
 
-    it 'cannot be created without first_name, last_name, phone number' do
-      @user.first_name = nil
-      @user.last_name = nil
-      expect(@user).to_not be_valid
-    end
+  describe 'validations' do
 
-    it 'cannot be created without phone number' do
-      @user.phone = nil
-      expect(@user).to_not be_valid
-    end
- end
+   it 'cannot be created without first_name, last_name, phone number' do
+        @user.first_name = nil
+        @user.last_name = nil
+        expect(@user).to_not be_valid
+      end
 
+      it 'cannot be created without phone number' do
+        @user.phone = nil
+        expect(@user).to_not be_valid
+      end
+
+      it 'requires the phone attr only contain numbers' do
+        @user.phone = 'mygreatstring'
+        expect(@user).to_not be_valid
+      end
+
+      it 'requires the phone attr to only have 10 chars' do
+        @user.phone = '54545454541'
+        expect(@user).to_not be_valid
+      end
+   end
+  end
   describe "custom name methods" do
     it 'has a full name method that combines first and last name' do
       expect(@user.full_name).to eq('ROSS, RICO')
