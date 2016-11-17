36fe590692fd432bd853724c65d7e4e2c3d2a476
Ryan Bent <ryanbent01@gmail.com>
2016-11-16 21:16:06 -0600

    Created Audit log resource and associated to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,6 +13,7 @@ class User < ActiveRecord::Base
   validates_format_of :phone, with: PHONE_REGEX
   validates :phone, length: { is: 10 }
   has_many :posts
+  has_many :audit_logs
 
   def full_name
     last_name.upcase + ", " + first_name.upcase
